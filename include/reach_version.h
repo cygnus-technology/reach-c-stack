d38302c1604d965b3edcf1abfe645d3d2a2f0380
chuckpeplinski <chuck.peplinski@i3pd.com>
2024-03-14 06:43:37 -0500

    preliminary version increment support

--- include/reach_version.h
+++ include/reach_version.h
@@ -1,13 +1,19 @@
 /*
- * reach_version.h
- *
- *  Version of reach-server
+ * reach_c_stack_version.h
+ *  Version of reach core code
  */
 
 #ifndef _REACH_VERSION_H_
 #define _REACH_VERSION_H_
 
-// Major version.Minor version.Build version with possible note
-#define REACH_VERSION_STRING "2.5.1"
+#define MAJOR_VERSION 2
+#define MINOR_VERSION 5
+#define BUILD_VERSION 2
+
+#ifdef BUILT_BY_PIPELINE 
+  #define PATCH_VERSION 0
+#else
+  #define PATCH_VERSION 255  // when built out of control
+#endif
 
 #endif  // ndef _REACH_VERSION_H_
